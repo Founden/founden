9a8673e51935c5ef61c034ff7bc31972370be433
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-04 16:42:14 +0200

    Skip validation of your locale

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -4,3 +4,6 @@ FastGettext.add_text_domain 'app', :path => 'locale'
 
 # Trust all usages of '%' in translations
 GettextI18nRails.translations_are_html_safe = true
+
+# Skip validation of your locale
+I18n.enforce_available_locales = false
