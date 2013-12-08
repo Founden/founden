61d42c8d2ba45ccf74f59ff5df0ef09919010f6a
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-08 16:53:36 +0200

    Api controllers now handle 404 errors as json response.

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -7,4 +7,11 @@ class Api::V1::ApplicationController < AuthenticatedController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery :with => :null_session
+
+  # Handle 404 errors
+  rescue_from(ActionController::RoutingError, ActiveRecord::RecordNotFound) do
+    render :status => :not_found, :json => {
+      :message => _('Resource unavailable')
+    }
+  end
 end
