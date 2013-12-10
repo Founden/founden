6f07b1cc84797e674ee6cb04ad3394bee91a2c80
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-10 20:26:30 +0200

    Cleanup deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,8 +7,8 @@ set :deploy_to, proc { '/home/deployer/%s' % fetch(:application) }
 set :scm, :git
 
 # set :format, :pretty
+set :log_level, :info
 set :log_level, :debug
-# set :log_level, :info
 # set :pty, true
 
 set :linked_files, %w{config/database.yml}
@@ -17,27 +17,13 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
-set :bundle_flags, '--deployment --quiet'
-
 set :rbenv_type, :user
 set :rbenv_ruby, '2.0.0-p353'
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 
