798ebf1e9d95dbe5d0c0be9d12b8973231f973ff
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-11 18:33:08 +0200

    Updated attachment fabricator.

--- spec/fabricators/attachment_fabricator.rb
+++ spec/fabricators/attachment_fabricator.rb
@@ -1,7 +1,10 @@
 Fabricator(:attachment) do
   title        { Faker::Lorem.sentence }
   user
-  network
-  conversation
-  message
+  network      { |attrs| Fabricate(:network, :user => attrs[:user]) }
+  conversation { |attrs| Fabricate(
+    :conversation, :user => attrs[:user], :network => attrs[:network]) }
+  message      { |attrs| Fabricate(
+    :message, :user => attrs[:user], :network => attrs[:network],
+    :conversation => attrs[:conversation]) }
 end
