a09446c5f513b1a283d03284c4fa7f5f66506e3c
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-13 21:11:36 +0200

    Move settings.yml to ignored files.

--- config/initializers/easy_auth.rb
+++ config/initializers/easy_auth.rb
@@ -1,7 +1,7 @@
 EasyAuth.config do |c|
   c.oauth2_client(
     :google,
-    Founden::Config.oauth2_providers.google.client_id,
-    Founden::Config.oauth2_providers.google.secret_key
+    Founden::Config.google_client_id,
+    Founden::Config.google_secret_key
   )
 end
