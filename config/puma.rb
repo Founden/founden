8fbd2e0c49c8d121e8939bb8fa8bbce612d1e6d5
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-22 21:12:38 +0200

    Fixed puma threading and cluster mode.

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,26 @@
-threads 1,1
-workers 1
-# Uncomment this if starting a cluster
+# Based on
+# https://github.com/puma/puma/blob/master/examples/config.rb
+
+# Configure “min” to be the minimum number of threads to use to answer
+# requests and “max” the maximum.
+#
+# The default is “0, 16”.
+#
+threads 0, 1
+
+# === Cluster mode ===
+
+# How many worker processes to run.
+#
+# The default is “0”.
+#
+# workers 0
+
+# If you're running in Clustered Mode you can optionally choose to preload
+# your application before starting up the workers.
+# This is necessary in order to take advantate of the Copy on Write feature
+# introduced in MRI Ruby 2.0.
+#
+# The default does not preload the application.
+#
 # preload_app!
