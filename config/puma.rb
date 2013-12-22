5b048c3b2457de3ff9cd32496c010f9be99e12fc
Stas SUȘCOV <stas@net.utcluj.ro>
2013-12-22 21:04:52 +0200

    Cleanup puma config file.

--- config/puma.rb
+++ config/puma.rb
@@ -2,9 +2,3 @@ threads 1,1
 workers 1
 # Uncomment this if starting a cluster
 # preload_app!
-
-on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
-  end
-end
