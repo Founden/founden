50c992c1abe7e4c6ed696356f8ddf22cd2a1d268
Stas SUȘCOV <stas@net.utcluj.ro>
2014-01-20 16:33:51 +0200

    Added tests for link attaching.

--- spec/features/link_spec.rb
+++ spec/features/link_spec.rb
@@ -4,7 +4,8 @@ feature 'Conversation link attachment', :js, :slow do
 
   given(:user) { User.first }
   given(:link) { Fabricate(:link, :user => user) }
-  given(:anchor) { '/conversations/%s' % link.conversation.slug }
+  given(:conversation) { link.conversation }
+  given(:anchor) { '/conversations/%s' % conversation.slug }
 
   background do
     try_google_sign_in
@@ -13,6 +14,28 @@ feature 'Conversation link attachment', :js, :slow do
 
   scenario 'details are shown' do
     expect(page).to have_content(link.url)
+  end
+
+  context 'is available to add' do
+    given(:conversation) { Fabricate(:conversation, :user => user) }
+    given(:content) { Faker::Lorem.sentence }
+
+    background do
+      page.find('.message-form-input').click
+      page.all('.caret-actions span').last.click
+      page.find('.attachment-options .link').click
+    end
+
+    scenario 'filling attachment editor saves it' do
+      page.fill_in('message', :with => content)
+      page.fill_in('link', :with => 'http://google.ro')
+      page.find('.message-form-actions button').trigger('click')
+      wait_for_ajax
+
+      expect(page).to have_content(content)
+      expect(page).to have_content('google.ro')
+    end
+
   end
 
 end
