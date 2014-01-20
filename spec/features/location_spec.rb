b1d63bddf4b48f2007f3547a6a8de68c65dffd21
Stas SUȘCOV <stas@net.utcluj.ro>
2014-01-20 16:34:05 +0200

    Added tests for location attaching.

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -4,7 +4,8 @@ feature 'Conversation location attachment', :js, :slow do
 
   given(:user) { User.first }
   given(:location) { Fabricate(:location, :user => user) }
-  given(:anchor) { '/conversations/%s' % location.conversation.slug }
+  given(:conversation) { location.conversation }
+  given(:anchor) { '/conversations/%s' % conversation.slug }
 
   background do
     try_google_sign_in
@@ -14,6 +15,33 @@ feature 'Conversation location attachment', :js, :slow do
   scenario 'map is shown' do
     expect(page).to have_content(location.title)
     expect(page).to have_css('.attachment-location .location-map')
+  end
+
+  context 'is available to add' do
+    given(:conversation) { Fabricate(:conversation, :user => user) }
+    given(:title) { Faker::Lorem.sentence }
+    given(:content) { Faker::Lorem.sentence }
+
+    background do
+      page.find('.message-form-input').click
+      page.all('.caret-actions span').last.click
+      page.find('.attachment-options .location').click
+    end
+
+    scenario 'filling attachment editor saves it' do
+      page.fill_in('message', :with => content)
+      page.find('.attachments-editor .enable-title').trigger('click')
+      page.fill_in('location', :with => 'Cluj, Romania')
+      sleep(1) # Wait map to load
+      page.fill_in('title', :with => title)
+      page.find('.message-form-actions button').trigger('click')
+      wait_for_ajax
+
+      expect(page).to have_content(title)
+      expect(page).to have_content(content)
+      expect(page).to have_css('.attachment-location .location-map')
+    end
+
   end
 
 end
